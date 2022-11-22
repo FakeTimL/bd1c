25264d933dd1234d7f56248f03e049b79d5804d7
Bingqi Li <faketiml@BingqsAir.local>
2022-11-22 21:38:29 +0000

    Initial version

---
+++ interpret.py
@@ -0,0 +1,37 @@
+with open('script.txt', 'r') as file:
+    lines = file.readlines()
+html = ''
+characters = dict()
+counts = dict()
+for line in lines:
+    if line.strip():
+        match line[0]:
+            case '#':
+                html += f'<div class="h1 row"><h1><p>{line[1:].strip()}</p></h1></div>\n'
+            case '【':
+                html += f'<div class="h2 row"><h2><p>{line.strip()}</p></h2></div>\n'
+            case '〔':
+                html += f'<div class="h3 row"><h3><p>{line.strip()}</p></h3></div>\n'
+            case '\'':
+                html += f'<div class="stage-direction row"><div class="character column"></div><div class="line column"><p>{line[1:].strip()}</p></div></div>\n'
+            case _:
+                character = line.split(':')[0]
+                names = character.split('@')[0].split('、')
+                for name in names:
+                    if name not in characters:
+                        characters.update({name: len(characters)})
+                        counts.update({name: 0})
+                    counts[name] += 1
+                classname = ' '.join(f"c{characters[name]}" for name in names)
+                displayname = character.split('@')[-1]
+                nameclass = 'wide ' if len(displayname)==2 or '、' in displayname else ('gapped ' if len(displayname)==3 else '')
+                html += f'<div class="dialogue row {classname}"><div class="character column"><p class="{nameclass}name">{displayname.replace("、","<br>")}</p></div><div class="line column"><p>{"：".join(line.split(":")[1:])}</p></div></div>\n'
+with open('script.html', 'w') as file:
+    file.write(html)
+
+items = ''
+for name in sorted(counts, key=counts.get, reverse=True):
+    items += f'<div class="item" id="c{characters[name]}" onclick="toggle(this);"><p>{name}</p></div>\n'
+    # print(characters[x], x, counts[x])
+with open('characters.html', 'w') as file:
+    file.write(items)
